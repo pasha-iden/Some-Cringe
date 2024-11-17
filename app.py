fd0799f1de195a9b9a4cdb7d699cb6493476331a
Pavel Didenko <brain_v3.6@mail.ru>
2024-11-17 22:42:31 +0300

    flask, library link, html

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print ("let's fuck'n do it")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/library')
+def index ():
+    return render_template("library.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)

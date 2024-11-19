a78bef4170d93fb867a70941fc3afd8a4db83b44
Pavel Didenko <brain_v3.6@mail.ru>
2024-11-19 20:11:09 +0300

    book information collection added, functions commented

--- tel.py
+++ tel.py
@@ -43,6 +43,7 @@ def bookslist():
     return books
 
 genres = genreslist()
+bookforadd=[0, 0, 0]
 
 @bot.message_handler(commands=['start'])
 def start (message):
@@ -52,9 +53,13 @@ def start (message):
 
 @bot.callback_query_handler(func = lambda callback:True)
 def buttoms (callback):
+
+    # Добавление книги 1: запрос указания названия книги
     if callback.data == 'adding':
         bot.send_message(callback.message.chat.id, 'Название книги')
         bot.register_next_step_handler(callback.message, adding_name)
+
+    # Просмотр списка книг
     if callback.data == 'watch':
         books = bookslist()
         bookslistprint=str()
@@ -64,19 +69,25 @@ def buttoms (callback):
         markup.add(types.InlineKeyboardButton('Посмотреть весь список', callback_data='watch'))
         markup.add(types.InlineKeyboardButton('Перейти на сайт', url='https://pashaiden.tilda.ws/biblioteka'))
         bot.send_message(callback.message.chat.id, 'Список: \n\n' + bookslistprint, reply_markup=markup)
+
+    # Добавление книги 4: добавление жанра, отчет о завершении добавления. Выбор дальнейшего действия.
     for i in range(len(genres)):
         if callback.data == genres[i][2]:
+            bookforadd[2] = genres[i][1]
             markup = types.InlineKeyboardMarkup()
             markup.add(types.InlineKeyboardButton('Посмотреть весь список', callback_data='watch'))
             markup.add(types.InlineKeyboardButton('Перейти на сайт', url='https://pashaiden.tilda.ws/biblioteka'))
-            bot.send_message(callback.message.chat.id, 'Книга добавлена', reply_markup=markup)
+            bot.send_message(callback.message.chat.id, 'Книга добавлена: \n\n' + bookforadd[0] + '\n' + bookforadd[1] + '\n' + bookforadd[2], reply_markup=markup)
 
-
+# Добавление книги 2: добавление названия книги, запрос указания автора
 def adding_name (message):
+    bookforadd[0]=message.text
     bot.send_message(message.chat.id, 'Автор')
     bot.register_next_step_handler(message, genre_name)
 
+# Добавление книги 3: добавление названия автора, запрос выбора жанра
 def genre_name (message):
+    bookforadd[1]=message.text
     markup = types.InlineKeyboardMarkup()
     for i in range(len(genres)):
         markup.add(types.InlineKeyboardButton(genres[i][1], callback_data=genres[i][2]))

4405dc0b17745be5da6ac2d6143b29690468b1d6
Pavel Didenko <brain_v3.6@mail.ru>
2024-11-20 16:38:48 +0300

    funcktions optimised, some tests added

--- tel.py
+++ tel.py
@@ -3,47 +3,38 @@ from telebot import types
 
 bot = telebot.TeleBot('7056258134:AAF1DhOPSw5Q9OT8jruWUbZ_xC5nSl3ia-U')
 
-def genreslist():
+
+def dbaction(selection, query):
     try:
         connection = psycopg2.connect(
-            host = "127.0.0.1",
-            user = "postgres",
-            password = "postgres",
-            database = "postgres")
+            host="127.0.0.1",
+            user="postgres",
+            password="postgres",
+            database="postgres")
         connection.autocommit = True
 
         with connection.cursor() as cursor:
-            cursor.execute('''SELECT * FROM genres''')
+            cursor.execute(query) # <--- сюда передается SQL запрос
             connection.commit()
-            genres=cursor.fetchall()
+            if selection==1:
+                datas=cursor.fetchall()
     except Exception as _ex:
         print ('Error:', _ex)
     finally:
         if connection:
             connection.close()
+            if selection==1:
+                return datas
+def genreslist():
+    genres=dbaction(1, '''SELECT * FROM genres''')
     return genres
 def bookslist():
-    try:
-        connection = psycopg2.connect(
-            host = "127.0.0.1",
-            user = "postgres",
-            password = "postgres",
-            database = "postgres")
-        connection.autocommit = True
-
-        with connection.cursor() as cursor:
-            cursor.execute('''SELECT * FROM books''')
-            connection.commit()
-            books=cursor.fetchall()
-    except Exception as _ex:
-        print ('Error:', _ex)
-    finally:
-        if connection:
-            connection.close()
+    books=(1, '''SELECT * FROM books''')
     return books
 
+
 genres = genreslist()
-bookforadd=[0, 0, 0]
+bookforadd=[0, 0, 0, 0]
 
 @bot.message_handler(commands=['start'])
 def start (message):
@@ -73,21 +64,22 @@ def buttoms (callback):
     # Добавление книги 4: добавление жанра, отчет о завершении добавления. Выбор дальнейшего действия.
     for i in range(len(genres)):
         if callback.data == genres[i][2]:
-            bookforadd[2] = genres[i][1]
+            bookforadd[3] = genres[i][1]
+
             markup = types.InlineKeyboardMarkup()
             markup.add(types.InlineKeyboardButton('Посмотреть весь список', callback_data='watch'))
             markup.add(types.InlineKeyboardButton('Перейти на сайт', url='https://pashaiden.tilda.ws/biblioteka'))
-            bot.send_message(callback.message.chat.id, 'Книга добавлена: \n\n' + bookforadd[0] + '\n' + bookforadd[1] + '\n' + bookforadd[2], reply_markup=markup)
+            bot.send_message(callback.message.chat.id, 'Книга добавлена: \n\n' + bookforadd[1] + '\n' + bookforadd[2] + '\n' + bookforadd[3] + '\n' + str(bookforadd[0]), reply_markup=markup)
 
 # Добавление книги 2: добавление названия книги, запрос указания автора
 def adding_name (message):
-    bookforadd[0]=message.text
+    bookforadd[1]=message.text
     bot.send_message(message.chat.id, 'Автор')
     bot.register_next_step_handler(message, genre_name)
 
-# Добавление книги 3: добавление названия автора, запрос выбора жанра
+# Добавление книги 3: добавление автора, запрос выбора жанра
 def genre_name (message):
-    bookforadd[1]=message.text
+    bookforadd[2]=message.text
     markup = types.InlineKeyboardMarkup()
     for i in range(len(genres)):
         markup.add(types.InlineKeyboardButton(genres[i][1], callback_data=genres[i][2]))
--- test.py
+++ test.py
@@ -1,26 +1,56 @@
 import psycopg2
 
+# a='Классическая художественная литература (зарубежная)'
+# try:
+#     connection = psycopg2.connect(
+#         host = "127.0.0.1",
+#         user = "postgres",
+#         password = "postgres",
+#         database = "postgres")
+#     connection.autocommit = True
+#
+#     with connection.cursor() as cursor:
+#         cursor.execute("""SELECT MAX(id) FROM books
+#         WHERE genre='%s'""" % (a))
+#         connection.commit()
+#         maxid=cursor.fetchone()
+#
+# except Exception as _ex:
+#     print ('Error:', _ex)
+# finally:
+#     if connection:
+#         connection.close()
 
-try:
-    connection = psycopg2.connect(
-        host = "127.0.0.1",
-        user = "postgres",
-        password = "postgres",
-        database = "postgres")
-    connection.autocommit = True
 
-    with connection.cursor() as cursor:
-        cursor.execute('''SELECT * FROM books''')
-        connection.commit()
-        genres=cursor.fetchall()
+# print(maxid[0])
 
-except Exception as _ex:
-    print ('Error:', _ex)
-finally:
-    if connection:
-        connection.close()
+# a=[3, 'a', 'b', 'c']
+#
+# try:
+#     connection = psycopg2.connect(
+#         host = "127.0.0.1",
+#         user = "postgres",
+#         password = "postgres",
+#         database = "postgres")
+#     connection.autocommit = True
+#
+#     with connection.cursor() as cursor:
+#         cursor.execute('''INSERT INTO books (id, bookname, author, genre)
+#         VALUES ('%s', '%s', '%s', '%s')''' % (a[0], a[1], a[2], a[3]))
+#         connection.commit()
+#
+# except Exception as _ex:
+#     print ('Error:', _ex)
+# finally:
+#     if connection:
+#         connection.close()
 
-print (genres)
-
-for i in range(len(genres)):
-    print(genres[i][1])
+# print (genres)
+#
+# for i in range(len(genres)):
+#     print(genres[i][1])
+#
+# b=tuple()
+# b=b+tuple('a')
+# b=b+tuple('b')
+# print(b)
